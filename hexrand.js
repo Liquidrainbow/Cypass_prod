272246a6c999e5e7c6cee5b35227de7a9c29e662
Liquidrainbow <piedcrust@gmail.com>
2024-11-10 13:19:15 +0300

    Some changes in js for hexrad

--- hexrand.js
+++ hexrand.js
@@ -1,68 +1,3 @@
-var hex = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
-
-// Linear congruential generator (LCG)
-function lcg(d) {
-	const a = 48271;
-	const m = 2 ** 31 - 1;
-	const q = Math.floor(m / a);
-	const r = m % a;
-
-	function getSeed() {
-		return (Math.floor(Math.random() * (m - 1)) + 1);
-	};
-
-	const s = Int32Array.of(getSeed(), 0, 0, 0);
-	function getNext(seed) {
-		if (seed !== undefined) s[0] = seed;
-		s[1] = s[0] / q;
-		s[2] = s[0] % q;
-		s[3] = a * s[2] - r * s[1];
-		s[0] = s[3] > 0 ? s[3] : s[3] + m;
-		return s[0];
-	}
-
-	var result = [];
-	for(var i = 0; i < d; i++) {
-		result += hex[Math.floor((getNext() / m) * 16)];
-	}
-	return result;
-}
-
-// Xorshift
-function Xorshift32(d) {
-	const m = 2 ** 32;
-	function getseed() {
-		return(Math.floor(Math.random() * (m - 1)) + 1);
-	}
-	const a = Uint32Array.of(getseed());
-
-	function getnext(seed) {
-    if (seed !== undefined) a[0] = seed;
-    a[0] ^= a[0] << 13;
-    a[0] ^= a[0] >>> 17;
-    a[0] ^= a[0] << 5;
-    return a[0];
-  }
-
-	var result = [];
-	for(var i = 0; i < d; i++) {
-		result += hex[Math.floor((getnext() / m) * 16)];
-	}
-	return result;
-}
-
-function Mathrand() {
-	return(Math.floor(Math.random()* 16));
-}
-
-// Crypto
-function Crypt() {
-	const m = 2 ** 32;
-  const buf = new Uint32Array(1);
-	const result = Math.floor((window.crypto.getRandomValues(buf)[0] / m)*16);
-	return result;
-}
-
 $(document).ready(function() {
 	$('#digits').on('keyup', function() {
 		var dmax = $('#digits').val();
@@ -158,3 +93,69 @@ $(document).ready(function() {
 	});
 
 });
+
+var hex = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+
+// Linear congruential generator (LCG)
+function lcg(d) {
+	const a = 48271;
+	const m = 2 ** 31 - 1;
+	const q = Math.floor(m / a);
+	const r = m % a;
+
+	function getSeed() {
+		return (Math.floor(Math.random() * (m - 1)) + 1);
+	};
+
+	const s = Int32Array.of(getSeed(), 0, 0, 0);
+	function getNext(seed) {
+		if (seed !== undefined) s[0] = seed;
+		s[1] = s[0] / q;
+		s[2] = s[0] % q;
+		s[3] = a * s[2] - r * s[1];
+		s[0] = s[3] > 0 ? s[3] : s[3] + m;
+		return s[0];
+	}
+
+	var result = [];
+	for(var i = 0; i < d; i++) {
+		result += hex[Math.floor((getNext() / m) * 16)];
+	}
+	return result;
+}
+
+// Xorshift
+function Xorshift32(d) {
+	const m = 2 ** 32;
+	function getseed() {
+		return(Math.floor(Math.random() * (m - 1)) + 1);
+	}
+	const a = Uint32Array.of(getseed());
+
+	function getnext(seed) {
+    if (seed !== undefined) a[0] = seed;
+    a[0] ^= a[0] << 13;
+    a[0] ^= a[0] >>> 17;
+    a[0] ^= a[0] << 5;
+    return a[0];
+  }
+
+	var result = [];
+	for(var i = 0; i < d; i++) {
+		result += hex[Math.floor((getnext() / m) * 16)];
+	}
+	return result;
+}
+
+function Mathrand() {
+	return(Math.floor(Math.random()* 16));
+}
+
+// Crypto
+function Crypt() {
+	const m = 2 ** 32;
+  const buf = new Uint32Array(1);
+	const result = Math.floor((window.crypto.getRandomValues(buf)[0] / m)*16);
+	return result;
+}
+
